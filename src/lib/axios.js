cf09e1a1c5140ef685e09c8e0d227c6f0160f963
Ahmed1khaled1 <ahmedkhaledg49@gmail.com>
2025-06-02 12:15:58 +0300

    11

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:"http://localhost:5000/api",
+  baseURL: "https://chat-backend-i7i5.vercel.app/api",
   withCredentials: true,
-})
+});
 
 export default axiosInstance;
